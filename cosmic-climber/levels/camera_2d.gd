926c69925b2e10bb351f27379bf82062612eee00
Marcus Paulsson <mapau990@student.liu.se>
2024-09-25 14:43:34 +0200

    minor cleanup

--- cosmic-climber/levels/camera_2d.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-@onready var player = $"../player"
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- cosmic-climber/menu_folder/main_menu.gd
+++ cosmic-climber/menu_folder/main_menu.gd
@@ -3,8 +3,8 @@ extends Control
 var GAME_STARTED = false
 
 # Debug variables
-var DEBUG = false
-var DEBUG_level = 1
+var DEBUG = true
+var DEBUG_level = 6
 
 @onready var name_dialog = $AcceptDialog  # Assuming NameDialog is a WindowDialog or AcceptDialog in the scene
 @onready var name_input = $AcceptDialog/LineEdit  # The input field inside the dialog for entering the name
